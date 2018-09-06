aecf5c1db6a9a6e65cc1ab3b845d2b9c9417a49c
Joel-An <rejoelve@gmail.com>
2018-09-06 21:38:42 +0900

    feat: change main contents

--- src/App.js
+++ src/App.js
@@ -3,16 +3,11 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var helloworld = 'Welcome to React';
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <h2>{helloworld}</h2>
+      </div>     
     );
   }
 }
