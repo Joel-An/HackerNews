b56b27474779fd99b06d08d180f49f6e21d7f351
Joel-An <rejoelve@gmail.com>
2018-09-08 16:25:30 +0900

    feat: add a button for each item
    The onDismiss() class method is not defined yet.

--- src/App.js
+++ src/App.js
@@ -41,6 +41,14 @@ class App extends Component {
                 <span>{item.author}</span>
                 <span>{item.num_comments}</span>
                 <span>{item.points}</span>
+                <span>
+                  <button
+                    onClick={()=> this.onDismiss(item.objectID)}
+                    type="button"
+                  >
+                    dismiss
+                  </button>
+                </span>
             </div>
         )}
       </div>     
