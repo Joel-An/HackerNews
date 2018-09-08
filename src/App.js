8afc9cba99a3bf4508e85cedcb9cad8abe798abf
Joel-An <rejoelve@gmail.com>
2018-09-08 18:50:08 +0900

    style: clean white space

--- src/App.js
+++ src/App.js
@@ -62,19 +62,19 @@ class App extends Component {
         {list.filter(isSearched(searchTerm)).map((item) =>
             <div key={item.objectID}>
               <span>
-                <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                <span>
-                  <button
-                    onClick={()=> this.onDismiss(item.objectID)}
-                    type="button"
-                  >
-                    dismiss
-                  </button>
-                </span>
+              <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={()=> this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                dismiss
+                </button>
+              </span>
             </div>
         )}
       </div>     
