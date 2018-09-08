17c03c95e204fd167acd70fc803d9a5a05e43e93
Joel-An <rejoelve@gmail.com>
2018-09-08 19:27:29 +0900

    feat: define a button component

--- src/App.js
+++ src/App.js
@@ -114,5 +114,25 @@ class Table extends Component {
   }
 }
 
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className,
+      children,
+    } = this.props;
+
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
 
 export default App;
