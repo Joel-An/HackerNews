1a419ce988dfcce2d0f6de23076ae84e644b78ac
Joel-An <rejoelve@gmail.com>
2018-09-08 19:17:17 +0900

    refactor: split up Table component

--- src/App.js
+++ src/App.js
@@ -57,25 +57,12 @@ class App extends Component {
           value={searchTerm}
           onChange={this.onSearchChange}
         />
-        {list.filter(isSearched(searchTerm)).map((item) =>
-            <div key={item.objectID}>
-              <span>
-              <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={()=> this.onDismiss(item.objectID)}
-                  type="button"
-                >
-                dismiss
-                </button>
-              </span>
-            </div>
-        )}
-      </div>     
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
     );
   }
 }
@@ -95,5 +82,35 @@ class Search extends Component {
     );
   }
 }
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map((item) =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                dismiss
+            </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
 
 export default App;
